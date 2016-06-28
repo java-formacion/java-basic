c1f560d22ed46221e61944712e6bb59b4470600f
JosebaZ <Curso@ForGrb-5.ipartekaula.com>
2016-06-28 09:16:24 +0200

    guardado copn fallos

--- src/com/ipartek/formacion/nombreproyecto/pojo/Candidato.java
+++ src/com/ipartek/formacion/nombreproyecto/pojo/Candidato.java
@@ -18,39 +18,89 @@ public class Candidato {
 	//atributos
 	private String nombre;
 	public String apellido1;
+	public String apellido2;
+	public String dni;
 	
 	
+	//constructor por defecto
 	
-	/**
-	 * recuperamos el nombre del objeto
-	 * @return
-	 */
+	public Candidato(){
+		super();
+	}
+	
+	public Candidato(String nombre, String dni) {
+		super();
+		this.nombre = nombre;
+		this.dni = dni;
+	}
+
+
+
+
 	public String getNombre() {
 		return nombre;
 	}
 
 
-	/**
-	 * Cambiamos el nombre pasandole un parametro
-	 * @param nombre <String> cadena de texto con el nombre del Candidato
-	 */
+
+
 	public void setNombre(String nombre) {
-		
-		
 		this.nombre = nombre;
 	}
 
 
-	public String apellido2;
-	public String dni;
-	
-	
-	//constructor
-	public Candidato(){
-		this.nombre = "";
-		this.apellido1 = "";
-		this.apellido2 = "";
-		this.dni = "";
+
+
+	public String getApellido1() {
+		return apellido1;
 	}
+
+
+
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+
+
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+
+
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+
+
+
+	public String getDni() {
+		return dni;
+	}
+
+
+
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+
+
+
+	@Override
+	public String toString() {
+		return "Candidato [nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2=" + apellido2 + ", dni=" + dni
+				+ "]";
+	}
+
+
+	
+
 	
 }
---
+++ src/com/ipartek/formacion/nombreproyecto/pojo/CandidatoTest.java
@@ -0,0 +1,32 @@
+package com.ipartek.formacion.nombreproyecto.pojo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CandidatoTest {
+
+	@Test
+	public void testCandidato() {
+		Candidato c = new Candidato("manolo", "xxxx");
+		
+		assertEquals("Manolo", c.getNombre();
+		
+	}
+
+	@Test
+	public void testGetNombre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetApellido1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetApellido1() {
+		fail("Not yet implemented");
+	}
+
+}
