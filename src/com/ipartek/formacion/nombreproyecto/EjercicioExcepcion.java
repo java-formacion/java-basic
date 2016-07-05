f22a5edfe91b0de43db9db7a7a0acc3b6039da4d
ffochox <ffochox@hotmail.com>
2016-07-05 10:27:33 +0200

    Add files via upload

---
+++ src/com/ipartek/formacion/nombreproyecto/EjercicioExcepcion.java
@@ -0,0 +1,57 @@
+package com.ipartek.formacion.nombreproyecto.ejercicios;
+
+public class EjercicioExcepcion {
+
+	public static void main(String[] args) {
+		try {
+			metodoA();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void metodoA() throws Exception {
+		metodoB();
+	}
+
+	public static void metodoB() throws Exception {
+		System.out.println("Iniciado metodo B");
+		metodoC();
+		System.out.println("Terminado metodo B");
+	}
+
+	/**
+	 * 
+	 * @throws Exception
+	 */
+	public static void metodoC() throws Exception {
+
+		System.out.println("Iniciado metodo C");
+		try{
+			
+			Object o = null;
+			o.toString();
+			//mas lineas...
+			System.out.println("Terminado correctamente metodo C");
+		
+		}catch(NullPointerException e){
+			System.out.println("Peta null");
+			
+		}catch(Exception e){		
+			
+			
+			e.printStackTrace();
+			//Escribir en fichero LOG
+			System.out.println("Escribir en Log " + e.getMessage() );
+			
+			throw new Exception("Lanzada adrede");
+			
+			
+		}finally {
+			System.out.println("Esto se ejecuta siempre, haya Excepcion o no");
+		}
+				
+	}
+
+}
