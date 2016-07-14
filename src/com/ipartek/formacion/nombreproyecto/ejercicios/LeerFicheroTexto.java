bf3c68b497695c9e2f9680136152d02498f52fb7
jonfaile <jonfraile82@gmail.com>
2016-07-14 13:26:48 +0200

    fichero

--- src/com/ipartek/formacion/nombreproyecto/ejercicios/LeerFicheroTexto.java
+++ src/com/ipartek/formacion/nombreproyecto/ejercicios/LeerFicheroTexto.java
@@ -3,6 +3,7 @@ package com.ipartek.formacion.nombreproyecto.ejercicios;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 
 public class LeerFicheroTexto {
 
@@ -13,20 +14,32 @@ public class LeerFicheroTexto {
 			BufferedReader br = new BufferedReader(fr);
 
 			String linea = "";
+
+			int contadorLeidas = 0;
+			int contadorErroneas = 0;
+			int registroErroneo = 0;
 			
-			int cont = 0;
+
 			while ((linea = br.readLine()) != null) {
 				System.out.println(linea);
-				
+
 				String campos[] = linea.split(",");
-				
-				//TODO crear objeto Persona
-				
-				cont++;
+
+				if (campos.length != 7) {
+					contadorErroneas++;
+					registroErroneo = contadorLeidas;
+				} else {
+					
+				}
+
+				contadorLeidas++;
+
 			}
 			System.out.println("---------------------------------");
-			System.out.println("Personas leidas " + cont);
-			System.out.println("Personas Erroneas " + cont);
+			System.out.println("Personas leidas " + contadorLeidas);
+			System.out.println("Personas Erroneas " + contadorErroneas);
+			System.out.println("Persona mal registada " + registroErroneo);
+
 			br.close();
 			fr.close();
 
